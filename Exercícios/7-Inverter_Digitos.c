f27d5d9249e8167688ea278d105d6fcee0a7bdc8
Asaph Arruda <160978598+ArrudaAsaph@users.noreply.github.com>
2024-10-16 13:02:16 +0000

    MUD: Separação dos executáveis

--- Exercícios/7-Inverter_Digitos.c
+++ Exercícios/7-Inverter_Digitos.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-int converter(int a){
-    int a1 = a / 100;
-    printf("%d",a1);
-    int a2 = (a / 10)%10;
-    printf("%d",a2);
-    int a3 = a % 10;
-    printf("%d",a3);
-    return a3,a2,a1;
-}
-
 int main(){
-    int a,a1,a2,a3;
-
+    int a;
+    printf("Digite um número: ");
     scanf("%d", &a);
-    a3,a2,a1 = converter(a);
-    printf("%d %d %d",a3,a2,a1);
+
+    int a1 = a / 100;
+
+    int a2 = (a / 10)%10;
+
+    int a3 = a % 10;
+  
+
+    printf("O número digitado invertido é igual a %d%d%d\n",a3,a2,a1);
 
 
     return 0;
