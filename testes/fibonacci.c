44023184a9a04de1f4902e70f4128fb9b5c78dca
ASAPH BONIFACIO GUIMARAES ARRUDA <20241014040010@ifrn.local>
2024-11-26 10:18:34 -0300

    Fibonacci

---
+++ testes/fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fibonacci(int termo){
+    int a, b, num_fib;
+
+    a = 0;
+    b = 1;
+
+    if (termo == 1){
+        num_fib = a;
+    } else if( termo == 2){
+        num_fib = b;
+    } else{
+        for (int i; )
+    }
+}
+
+int main(){
+    return 0;
+}
