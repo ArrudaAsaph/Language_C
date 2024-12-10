0b02164fd105e7227b32c460b67f070d0ec1a513
Asaph Arruda <160978598+ArrudaAsaph@users.noreply.github.com>
2024-12-10 00:47:30 +0000

    Teste alocação

--- struct/agenda.c
+++ struct/agenda.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Agenda{
     char nome[50];
@@ -17,13 +18,44 @@ struct Agenda* criar_contato(char nome[], char telefone[]){
 
 }
 
-void criar_contato(int op){
-    char nome[50];
-    printf("Nome do novo contato: ");
-    scanf("%s",nome);
-}
 int main(){
+    int  op = 0;
+    char nome[50];
+    char telefone[15];
 
+    while (op != -1){
+        printf("Escolha a operação:\n");
+        printf("1 - Inserir Cliente; 2 - Atualizar Cliente; 3 - Listar Clientes; 4 - Remover Cliente; -1 - Sair\n");
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 1:
+        
+            printf("\nNome: ");
+            scanf("%s", nome);
+            printf("\nTelefone: ");
+            scanf("%s", telefone);
+            struct Agenda* agenda = criar_contato(nome,telefone);
+            if (agenda != NULL) {
+                printf("Contato criado: %s, %s\n", agenda->nome, agenda->telefone);
+                free(agenda); // Libera a memória após o uso
+            }
+            break;
+        case 2:
+        
+            break;
+        
+        case 3:
+            /* code */
+            break;
+        case 4:
+
+            break;
+        
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
